2455263303ead473bef909d378b4ef7ffe493df3
Isyohan <78814275+Isyohan@users.noreply.github.com>
2021-04-23 18:07:02 +0200

    Add files via upload
    second prototype of the game.
    1 type of enemy , 1 type of attack.
    enemies just jump.
    we need more levels ,musics and better graphics for the environment.

---
+++ Game1/src/main/java/Main/Type.java
@@ -0,0 +1,9 @@
+package Main;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public enum Type {
+    PLAYER, COIN, PLATFORM, EXIT_TRIGGER, ENEMY, KEY_PROMPT, EXIT_SIGN, BUTTON,
+    DOOR_TOP, DOOR_BOT, MESSAGE_PROMPT, MUSIC
+}
---
+++ Game1/src/main/java/components/PlayerComponent.java
@@ -0,0 +1,111 @@
+package components;
+
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.texture.AnimatedTexture;
+import com.almasb.fxgl.texture.AnimationChannel;
+import javafx.geometry.Point2D;
+import javafx.scene.image.Image;
+import javafx.util.Duration;
+
+import static com.almasb.fxgl.dsl.FXGL.image;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.play;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public class PlayerComponent extends Component {
+
+    private PhysicsComponent physics;
+
+    private AnimatedTexture texture;
+
+    private AnimationChannel animIdle, animWalk;
+
+    private int jumps = 2;
+
+    public PlayerComponent() {
+
+        Image image = image("player.png");
+
+        animIdle = new AnimationChannel(image, 4, 32, 42, Duration.seconds(1), 1, 1);
+        animWalk = new AnimationChannel(image, 4, 32, 42, Duration.seconds(0.66), 0, 3);
+
+        texture = new AnimatedTexture(animIdle);
+        texture.loop();
+    }
+
+    @Override
+    public void onAdded() {
+        entity.getTransformComponent().setScaleOrigin(new Point2D(16, 21));
+        entity.getViewComponent().addChild(texture);
+
+        physics.onGroundProperty().addListener((obs, old, isOnGround) -> {
+            if (isOnGround) {
+                play("land.wav");
+                jumps = 2;
+            }
+        });
+    }
+
+    @Override
+    public void onUpdate(double tpf) {
+        if (physics.isMovingX()) {
+            if (texture.getAnimationChannel() != animWalk) {
+                texture.loopAnimationChannel(animWalk);
+            }
+        } else {
+            if (texture.getAnimationChannel() != animIdle) {
+                texture.loopAnimationChannel(animIdle);
+            }
+        }
+    }
+
+    public void left() {
+        getEntity().setScaleX(-1);
+        physics.setVelocityX(-170);
+    }
+
+    public void right() {
+        getEntity().setScaleX(1);
+        physics.setVelocityX(170);
+    }
+
+    public void stop() {
+        physics.setVelocityX(0);
+    }
+
+    public void jump() {
+        if (jumps == 0)
+            return;
+
+        play("jump.wav");
+        physics.setVelocityY(-300);
+
+        jumps--;
+    }
+
+    public void whistle() {
+
+        play("Honking.wav");
+
+    }
+    public double dEnemy(EnemyComponent enemy){
+        double d=0;
+
+
+        return d;
+    }
+
+    public Boolean closeEnough(EnemyComponent en,double dMax){
+        Point2D pe=en.getEntity().getPosition();
+        Point2D pp=this.getEntity().getPosition();
+        double dX=Math.abs(pp.getX()-pe.getX());
+        double dY=Math.abs(pp.getY()-pe.getY());
+        double d=Math.sqrt(dY*dY+dX*dX);
+        if (d<dMax){
+            return true;
+        }
+        return false;
+    }
+}
