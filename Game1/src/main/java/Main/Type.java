30e52d0e77f9e035a06096a3a6140da9568bb876
Isyohan <78814275+Isyohan@users.noreply.github.com>
2021-05-22 21:27:25 +0200

    Delete Game1 directory

--- Game1/src/main/java/Main/Type.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-/**
- * @author Almas Baimagambetov (almaslvl@gmail.com)
- */
-public enum Type {
-    PLAYER, COIN, PLATFORM, EXIT_TRIGGER, ENEMY, KEY_PROMPT, EXIT_SIGN, BUTTON,
-    DOOR_TOP, DOOR_BOT, MESSAGE_PROMPT, MUSIC
-}
